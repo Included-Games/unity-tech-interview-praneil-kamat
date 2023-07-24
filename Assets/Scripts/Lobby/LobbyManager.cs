406a66154b97d3f108ce699454198fb46f481772
Francesco Puglisi <francesco@included.games>
2023-07-24 14:57:12 +0100

    Define methods to create and join lobby

--- Assets/Scripts/Lobby/LobbyManager.cs
+++ Assets/Scripts/Lobby/LobbyManager.cs
@@ -4,6 +4,19 @@ namespace Lobby
 {
     public class LobbyManager
     {
-        
+        public Unity.Services.Lobbies.Models.Lobby CreateLobby()
+        {
+            var task = Lobbies.Instance.CreateLobbyAsync("MyLobby", 4, new CreateLobbyOptions()
+            {
+                IsPrivate = true
+            });
+            return task.GetAwaiter().GetResult();
+        }
+
+        public Unity.Services.Lobbies.Models.Lobby JoinLobby(string lobbyCode)
+        {
+            var task = Lobbies.Instance.JoinLobbyByCodeAsync(lobbyCode);
+            return task.GetAwaiter().GetResult();
+        }
     }
 }
