cdac45fdfe5feadfd1d2111b9d4bd14d8fc644db
Francesco Puglisi <francesco@included.games>
2023-07-24 14:57:45 +0100

    Define methods to poll lobby and keep track of current lobby

--- Assets/Scripts/Lobby/LobbyManager.cs
+++ Assets/Scripts/Lobby/LobbyManager.cs
@@ -1,22 +1,49 @@
+using System.Collections;
 using Unity.Services.Lobbies;
+using UnityEngine;
 
 namespace Lobby
 {
-    public class LobbyManager
+    public class LobbyManager : MonoBehaviour
     {
+        private Unity.Services.Lobbies.Models.Lobby CurrentLobby { get; set; }
+        private string lobbyId;
+        
         public Unity.Services.Lobbies.Models.Lobby CreateLobby()
         {
             var task = Lobbies.Instance.CreateLobbyAsync("MyLobby", 4, new CreateLobbyOptions()
             {
                 IsPrivate = true
             });
-            return task.GetAwaiter().GetResult();
+            var lobby = task.GetAwaiter().GetResult();
+            CurrentLobby = lobby;
+            lobbyId = lobby.Id;
+            StartCoroutine(GetLobby(2.5f));
+            return lobby;
         }
 
         public Unity.Services.Lobbies.Models.Lobby JoinLobby(string lobbyCode)
         {
             var task = Lobbies.Instance.JoinLobbyByCodeAsync(lobbyCode);
-            return task.GetAwaiter().GetResult();
+            var lobby = task.GetAwaiter().GetResult();
+            CurrentLobby = lobby;
+            lobbyId = lobby.Id;
+            StartCoroutine(GetLobby(0.1f));
+            return lobby;
+        }
+        
+        private void GetLobby()
+        {
+            var task = Lobbies.Instance.GetLobbyAsync(lobbyId);
+            var lobby = task.GetAwaiter().GetResult();
+            StartCoroutine(GetLobby(0.0001f));
+        }
+        
+        private IEnumerator GetLobby(float delay)
+        {
+            yield return new WaitForSeconds(delay);
+
+            GetLobby();
         }
     }
 }
